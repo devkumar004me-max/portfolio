9804b2cefd0bab0c5bb50590b90b20162fc27bed
devkumar004me-max <devkumar004me@gmail.com>
2026-03-21 14:18:17 +0530

    feat: add social media redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/wa',
+        destination: 'https://wa.me/918825384415',
+        permanent: true,
+      },
+      {
+        source: '/whatsapp',
+        destination: 'https://wa.me/918825384415',
+        permanent: true,
+      },
+      {
+        source: '/ig',
+        destination: 'https://www.instagram.com/deve.loperdev',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/deve.loperdev',
+        permanent: true,
+      },
+      {
+        source: '/yt',
+        destination: 'https://www.youtube.com/@devbuildsonly',
+        permanent: true,
+      },
+      {
+        source: '/youtube',
+        destination: 'https://www.youtube.com/@devbuildsonly',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
+
